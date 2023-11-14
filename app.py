ce98bffd21ff990f63d5eaf55b7069132ff328da
RomanLukianchyk <19arrow19@gmail.com>
2023-11-14 19:23:44 +0100

    Change to connection

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from pathlib import Path
 from main_functions.CLI import main_cli
 from flask import Flask, request, render_template, jsonify
 from flask_restful import Api, Resource, reqparse
-from flasgger import Swagger, swag_from
+from flasgger import Swagger
+import connexion
 import xml.etree.ElementTree as ET
+
+app = connexion.FlaskApp(__name__, specification_dir='.')
+app.add_api('swagger.yml')
 
 app = Flask(__name__)
 api = Api(app)
 swagger = Swagger(app)
+
 
 parser = reqparse.RequestParser()
 parser.add_argument('version', type=int, help='API version', required=True)
@@ -15,7 +20,6 @@ parser.add_argument('format', type=str, help='Response format (JSON, XML)', requ
 
 
 class ReportResource(Resource):
-    @swag_from('my_resource.yml')
     def get(self):
         version = int(request.args.get('version'))
         format = request.args.get('format')
@@ -75,33 +79,33 @@ class ReportResource(Resource):
 api.add_resource(ReportResource, '/api/v1/report/')
 
 
-@app.route('/report')
+@app.route('/api/report')
 def show_common_report():
     best_racers, invalid_racers = main_cli(files=Path("data"), sort_order=None)
     return render_template('common_report.html', best_racers=best_racers, invalid_racers=invalid_racers)
 
 
-@app.route('/report/drivers')
+@app.route('/api/report/drivers')
 def show_report():
     order = request.args.get('order', 'asc')
     best_racers, invalid_racers = main_cli(files=Path("data"), sort_order=order)
     return render_template('ordered_report.html', best_racers=best_racers, invalid_racers=invalid_racers)
 
 
-@app.route('/report/driver')
+@app.route('/api/report/driver')
 def show_driver():
     driver_id = request.args.get('driver_id')
     best_racers, invalid_racers = main_cli(files=Path("data"), driver_name=driver_id)
     if not driver_id:
-        return ("Usege: /report/driver?driver_id=SVF")
+        return "Usege: /report/driver?driver_id=SVF"
     return render_template('driver_report.html', best_racers=best_racers, invalid_racers=invalid_racers)
 
 
-@app.route('/driver_list')
+@app.route('/api/driver_list')
 def show_driver_list():
     data = main_cli(files=Path("data"), list_drivers=True)
     return render_template('driver_list.html', data=data)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
