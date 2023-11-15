4077a59b0c7498274624a3e27c3cfd18fc8b29e9
RomanLukianchyk <19arrow19@gmail.com>
2023-11-15 13:18:43 +0100

    Api without yaml

--- app.py
+++ app.py
@@ -3,16 +3,20 @@ from main_functions.CLI import main_cli
 from flask import Flask, request, render_template, jsonify
 from flask_restful import Api, Resource, reqparse
 from flasgger import Swagger
-import connexion
 import xml.etree.ElementTree as ET
-
-app = connexion.FlaskApp(__name__, specification_dir='.')
-app.add_api('swagger.yml')
 
 app = Flask(__name__)
 api = Api(app)
-swagger = Swagger(app)
+swagger_template = {
+    "swagger": "2.0",
+    "info": {
+        "title": "Race Report API",
+        "description": "API for accessing race report data",
+        "version": "0.0.1"
+    },
+}
 
+swagger = Swagger(app, template=swagger_template)
 
 parser = reqparse.RequestParser()
 parser.add_argument('version', type=int, help='API version', required=True)
@@ -21,8 +25,39 @@ parser.add_argument('format', type=str, help='Response format (JSON, XML)', requ
 
 class ReportResource(Resource):
     def get(self):
-        version = int(request.args.get('version'))
-        format = request.args.get('format')
+        """
+        Get Race Report
+        ---
+        tags:
+          - Report
+        parameters:
+          - name: version
+            in: query
+            type: integer
+            required: true
+            description: API version (e.g., 1)
+          - name: format
+            in: query
+            type: string
+            required: true
+            description: Response format (JSON, XML)
+        responses:
+          '200':
+            description: Successful response
+            schema:
+              type: object
+              properties:
+                best_racers:
+                  type: object
+                  description: Best racers data
+                invalid_racers:
+                  type: object
+                  description: Invalid racers data
+          '400':
+            description: Bad Request
+        """
+        version = int(request.args.get('version', 1))
+        format = request.args.get('format', 'JSON')
 
         if version == 1:
             best_racers, invalid_racers = main_cli(files=Path("data"), sort_order=None)
@@ -79,21 +114,72 @@ class ReportResource(Resource):
 api.add_resource(ReportResource, '/api/v1/report/')
 
 
-@app.route('/api/report')
+@app.route('/report')
 def show_common_report():
+    """
+    Show Common Report
+    ---
+    tags:
+      - Report
+    summary: Show Common Report
+    description: This endpoint renders a common HTML report.
+    responses:
+      '200':
+        description: Successful response
+    parameters:
+      - name: order
+        in: query
+        type: string
+        required: false
+        description: Order for the report (asc, desc)
+    """
     best_racers, invalid_racers = main_cli(files=Path("data"), sort_order=None)
     return render_template('common_report.html', best_racers=best_racers, invalid_racers=invalid_racers)
 
 
-@app.route('/api/report/drivers')
+@app.route('/report/drivers')
 def show_report():
+    """
+    Show Report
+    ---
+    tags:
+      - Report
+    summary: Show Report
+    description: This endpoint renders an ordered HTML report.
+    responses:
+      '200':
+        description: Successful response
+    parameters:
+      - name: order
+        in: query
+        type: string
+        required: false
+        description: Order for the report (asc, desc)
+    """
     order = request.args.get('order', 'asc')
     best_racers, invalid_racers = main_cli(files=Path("data"), sort_order=order)
     return render_template('ordered_report.html', best_racers=best_racers, invalid_racers=invalid_racers)
 
 
-@app.route('/api/report/driver')
+@app.route('/report/driver')
 def show_driver():
+    """
+    Show Driver Report
+    ---
+    tags:
+      - Report
+    summary: Show Driver Report
+    description: This endpoint renders an HTML report for a specific driver.
+    responses:
+      '200':
+        description: Successful response
+    parameters:
+      - name: driver_id
+        in: query
+        type: string
+        required: false
+        description: Driver ID (e.g., SVF)
+    """
     driver_id = request.args.get('driver_id')
     best_racers, invalid_racers = main_cli(files=Path("data"), driver_name=driver_id)
     if not driver_id:
@@ -101,11 +187,22 @@ def show_driver():
     return render_template('driver_report.html', best_racers=best_racers, invalid_racers=invalid_racers)
 
 
-@app.route('/api/driver_list')
+@app.route('/driver_list')
 def show_driver_list():
+    """
+    Show Driver List
+    ---
+    tags:
+      - Report
+    summary: Show Driver List
+    description: This endpoint renders a list of drivers in HTML.
+    responses:
+      '200':
+        description: Successful response
+    """
     data = main_cli(files=Path("data"), list_drivers=True)
     return render_template('driver_list.html', data=data)
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(debug=True)
